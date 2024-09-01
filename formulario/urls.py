abc7ea60f852ab9972511fc795b826ae51c1fd0b
Leandro Fernandez <leandroezequielfernandez136@gmail.com>
2024-08-31 23:24:15 -0300

    palabras corregidas 4

--- formulario/urls.py
+++ formulario/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import inscripcion_view, gracias_view
 
 urlpatterns = [
-    path('inscripcion/', inscripcion_view, name='inscripcion'),
+    path('', inscripcion_view, name='inscripcion'),
      path('gracias/', gracias_view, name='gracias'),
 ]
