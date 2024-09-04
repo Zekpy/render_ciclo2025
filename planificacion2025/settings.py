38d013e8514b80b04955e3146d0de372eeed4ac6
Leandro Fernandez <leandroezequielfernandez136@gmail.com>
2024-09-04 12:31:47 -0300

    Carga de cursos completada con imagenes y tambien configuracion de render 2

--- planificacion2025/settings.py
+++ planificacion2025/settings.py
@@ -132,3 +132,6 @@ if not DEBUG:
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Ruta para archivos multimedia (imágenes, vídeos, etc.)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'cursos_imagenes')
