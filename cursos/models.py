d981950f19d37a54b190aada70a2fedee3ad3279
Leandro Fernandez <leandroezequielfernandez136@gmail.com>
2024-09-06 06:38:40 -0300

    cambios de url en imagenes de cursos

--- cursos/models.py
+++ cursos/models.py
@@ -10,6 +10,6 @@ class Curso(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
     categoria = models.ForeignKey(Categoria, null=True, blank=True, on_delete=models.SET_NULL, default=None)
-    imagen = models.ImageField(upload_to='static/cursos', blank=True, null=True)
+    imagen_url = models.URLField(max_length=500, blank=True)  # Campo de texto para la URL de la imagen
     def __str__(self):
         return self.nombre
